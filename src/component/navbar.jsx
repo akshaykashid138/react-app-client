5b22ba368123cbcc3ad7c1c0fd5fa3747724f305
akshay138 <kashid.akshay138@gmail.com>
2021-03-24 22:46:04 +0530

    changes

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,14 +4,14 @@ function Navbar() {
     return (
         <div className='container'>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="/">Home</a>
+                <a class="navbar-brand" href="https://pedantic-allen-916df9.netlify.app">Home</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="https://pedantic-allen-916df9.netlify.app/login">Register</a>
-                    <a class="nav-item nav-link" href="https://pedantic-allen-916df9.netlify.app/login">Login</a>
+                    <a class="nav-item nav-link" href="/register">Register</a>
+                    <a class="nav-item nav-link" href="/login">Login</a>
                     
                     </div>
                 </div>
