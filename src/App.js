1807896ba74c82fb9f2a3a847d0eb327efa6fa12
akshay138 <kashid.akshay138@gmail.com>
2021-03-27 14:11:57 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Registration from './component/Registration'
 import Login from './component/login'
 import {Route} from 'react-router-dom'
 import HomeScreen from './component/homeScreen'
-import Navbar from './component/navbar'
+// import Navbar from './component/navbar'
 import './App.css';
 
 function App() {
   return (
     <>
-    <Navbar />
+    {/* <Navbar /> */}
     <Route path="/register" component={Registration} />
     <Route path="/login" component={Login} />
     <Route path='/homeScreen' component={HomeScreen} />
